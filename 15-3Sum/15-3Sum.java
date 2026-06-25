73983183e7bb0cf81c2f7f75e5a2bc2fce906a89
Samritan <samritanbasotra1@gmail.com>
2026-06-25 15:56:53 +0530

    File updated at 25/06/2026, 15:56:54

--- 15-3Sum/15-3Sum.java
+++ 15-3Sum/15-3Sum.java
@@ -1,45 +1,38 @@
-// Last updated: 25/06/2026, 15:49:21
-1class Solution {
-2    public List<List<Integer>> threeSum(int[] nums) {
-3        List<List<Integer>> result=new ArrayList<>();
-4        Arrays.sort(nums);
-5        for(int i=0;i<nums.length-2;i++){
-6            if(i>0 && nums[i]==nums[i-1]){
-7                continue;
-8            }
-9            int left=i+1;
-10            int right=nums.length-1;
-11            while(left<right){
-12         int sum=nums[i]+nums[left]+nums[right];
-13         if (sum == 0) {
-14
-15                    result.add(Arrays.asList(
-16                            nums[i],
-17                            nums[left],
-18                            nums[right]
-19                    ));
-20
-21                    left++;
-22                    right--;
-23                  while (left < right &&
-24                     nums[left] == nums[left - 1]) {
-25                       left++;
-26                    }
-27                    while (left < right &&
-28                           nums[right] == nums[right + 1]) {
-29                        right--;
-30                    }
-31
-32                 
-33            }else if (sum<0){
-34                left++;
-35            }else{
-36                right--;
-37            }
-38
-39        }
-40        
-41    }
-42    return result;
-43}
-44}
+// Last updated: 25/06/2026, 15:56:54
+1
+2       
+3
+4class Solution {
+5    public int threeSumClosest(int[] nums, int target) {
+6
+7        Arrays.sort(nums);
+8
+9        int closest = nums[0] + nums[1] + nums[2];
+10
+11        for (int i = 0; i < nums.length - 2; i++) {
+12
+13            int left = i + 1;
+14            int right = nums.length - 1;
+15
+16            while (left < right) {
+17
+18                int sum = nums[i] + nums[left] + nums[right];
+19
+20                if (Math.abs(sum - target) <
+21                    Math.abs(closest - target)) {
+22                    closest = sum;
+23                }
+24
+25                if (sum < target) {
+26                    left++;
+27                } else if (sum > target) {
+28                    right--;
+29                } else {
+30                    return sum; 
+31                }
+32            }
+33        }
+34
+35        return closest;
+36    }
+37}
