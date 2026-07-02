19607c322acfb5fc1d7134bcec7c0d30243235a9
Samritan <samritanbasotra1@gmail.com>
2026-07-03 00:01:41 +0530

    File updated at 03/07/2026, 00:01:41

--- 155-MinStack/155-MinStack.java
+++ 155-MinStack/155-MinStack.java
@@ -1,48 +1,35 @@
-// Last updated: 02/07/2026, 23:51:40
-1class MinStack {
-2    private Stack<Integer> minstack;
-3    private Stack<Integer> stack;
-4    public MinStack() {
-5          stack = new Stack<>();
-6        minstack = new Stack<>();
-7        
-8    }
-9    
-10    public void push(int val) {
-11          stack.push(val);
-12
-13        if (minstack.isEmpty()) {
-14            minstack.push(val);
-15        } else {
-16            minstack.push(Math.min(val, minstack.peek()));
-17        }
-18        
-19    }
-20    
-21    public void pop() {
-22         stack.pop();
-23        minstack.pop();
-24        
-25
-26    }
-27    
-28    public int top() {
-29          return stack.peek();
-30        
-31    }
-32    
-33    public int getMin() {
-34                return minstack.peek();
-35
-36        
-37    }
-38}
-39
-40/**
-41 * Your MinStack object will be instantiated and called as such:
-42 * MinStack obj = new MinStack();
-43 * obj.push(value);
-44 * obj.pop();
-45 * int param_3 = obj.top();
-46 * int param_4 = obj.getMin();
-47 */
+// Last updated: 03/07/2026, 00:01:41
+1import java.util.*;
+2
+3class Solution {
+4    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+5
+6        Stack<Integer> stack = new Stack<>();
+7        HashMap<Integer, Integer> map = new HashMap<>();
+8
+9
+10        for (int i = nums2.length - 1; i >= 0; i--) {
+11
+12            while (!stack.isEmpty() && stack.peek() <= nums2[i]) {
+13                stack.pop();
+14            }
+15
+16            if (stack.isEmpty()) {
+17                map.put(nums2[i], -1);
+18            } else {
+19                map.put(nums2[i], stack.peek());
+20            }
+21
+22            stack.push(nums2[i]);
+23        }
+24
+25        
+26        int[] ans = new int[nums1.length];
+27
+28        for (int i = 0; i < nums1.length; i++) {
+29            ans[i] = map.get(nums1[i]);
+30        }
+31
+32        return ans;
+33    }
+34}
