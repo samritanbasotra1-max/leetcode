5fcfd8676ab0786c2d1bb510b7996021d9de8b28
Samritan <samritanbasotra1@gmail.com>
2026-07-14 18:00:09 +0530

    File updated at 14/07/2026, 18:00:08

--- 144-BinaryTreePreorderTraversal/144-BinaryTreePreorderTraversal.java
+++ 144-BinaryTreePreorderTraversal/144-BinaryTreePreorderTraversal.java
@@ -1,40 +1,24 @@
-// Last updated: 14/07/2026, 17:58:40
-1/**
-2 * Definition for a binary tree node.
-3 * public class TreeNode {
-4 *     int val;
-5 *     TreeNode left;
-6 *     TreeNode right;
-7 *     TreeNode() {}
-8 *     TreeNode(int val) { this.val = val; }
-9 *     TreeNode(int val, TreeNode left, TreeNode right) {
-10 *         this.val = val;
-11 *         this.left = left;
-12 *         this.right = right;
-13 *     }
-14 * }
-15 */
+// Last updated: 14/07/2026, 18:00:08
+1class Solution {
+2
+3    List<Integer> ans = new ArrayList<>();
+4
+5    public List<Integer> inorderTraversal(TreeNode root) {
+6
+7        inorder(root);
+8
+9        return ans;
+10    }
+11
+12    private void inorder(TreeNode root) {
+13
+14        if (root == null)
+15            return;
 16
-17class Solution {
+17        inorder(root.left);
 18
-19    List<Integer> ans = new ArrayList<>();
+19        ans.add(root.val);
 20
-21    public List<Integer> preorderTraversal(TreeNode root) {
-22
-23        preorder(root);
-24
-25        return ans;
-26    }
-27
-28    private void preorder(TreeNode root) {
-29
-30        if (root == null)
-31            return;
-32
-33        ans.add(root.val);
-34
-35        preorder(root.left);
-36
-37        preorder(root.right);
-38    }
-39}
+21        inorder(root.right);
+22    }
+23}
