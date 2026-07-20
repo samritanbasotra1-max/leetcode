a0bf7db47b75b96d24e924395eb53060589c1386
Samritan <samritanbasotra1@gmail.com>
2026-07-20 21:18:19 +0530

    Create 509-FibonacciNumber

---
+++ 509-FibonacciNumber/509-FibonacciNumber.java
@@ -0,0 +1,14 @@
+// Last updated: 20/07/2026, 21:18:18
+1class Solution {
+2    public int fib(int n) {
+3        if(n==0||n==1)
+4        return n;
+5
+6        return fib(n-1)+fib(n-2);
+7       
+8
+9        }
+10        
+11
+12    }
+13
