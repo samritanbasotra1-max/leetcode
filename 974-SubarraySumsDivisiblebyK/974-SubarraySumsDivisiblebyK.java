211c47dab282b223f61725d5388068f27379693d
Samritan <samritanbasotra1@gmail.com>
2026-07-29 22:48:58 +0530

    File updated at 29/07/2026, 22:48:58

--- 974-SubarraySumsDivisiblebyK/974-SubarraySumsDivisiblebyK.java
+++ 974-SubarraySumsDivisiblebyK/974-SubarraySumsDivisiblebyK.java
@@ -1,25 +1,31 @@
-// Last updated: 29/07/2026, 22:46:04
+// Last updated: 29/07/2026, 22:48:58
 1class Solution {
-2    public int numSubarraysWithSum(int[] nums, int goal) {
-3
-4        HashMap<Integer, Integer> map = new HashMap<>();
-5
-6        map.put(0, 1);
-7
-8        int sum = 0;
-9        int count = 0;
-10
-11        for (int num : nums) {
-12
-13            sum += num;
-14
-15            if (map.containsKey(sum - goal)) {
-16                count += map.get(sum - goal);
-17            }
-18
-19            map.put(sum, map.getOrDefault(sum, 0) + 1);
-20        }
-21
-22        return count;
-23    }
-24}
+2
+3    public int numSubarraysWithSum(int[] nums, int goal) {
+4        return atMost(nums, goal) - atMost(nums, goal - 1);
+5    }
+6
+7    private int atMost(int[] nums, int goal) {
+8
+9        if (goal < 0)
+10            return 0;
+11
+12        int left = 0;
+13        int sum = 0;
+14        int count = 0;
+15
+16        for (int right = 0; right < nums.length; right++) {
+17
+18            sum += nums[right];
+19
+20            while (sum > goal) {
+21                sum -= nums[left];
+22                left++;
+23            }
+24
+25            count += right - left + 1;
+26        }
+27
+28        return count;
+29    }
+30}
