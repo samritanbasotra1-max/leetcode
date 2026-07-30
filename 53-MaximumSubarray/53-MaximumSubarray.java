02cb2132614b13cae92c8c7e82fcd279138c33b9
Samritan <samritanbasotra1@gmail.com>
2026-07-30 23:40:40 +0530

    File updated at 30/07/2026, 23:40:39

--- 53-MaximumSubarray/53-MaximumSubarray.java
+++ 53-MaximumSubarray/53-MaximumSubarray.java
@@ -1,33 +1,27 @@
-// Last updated: 30/07/2026, 23:31:41
+// Last updated: 30/07/2026, 23:40:39
 1class Solution {
-2
-3    public int maxSubarraySumCircular(int[] nums) {
-4
-5        int totalSum = 0;
-6
-7        int currentMax = nums[0];
-8        int maxSum = nums[0];
+2    public int maxProduct(int[] nums) {
+3
+4        int maxProduct = nums[0];
+5        int minProduct = nums[0];
+6        int ans = nums[0];
+7
+8        for (int i = 1; i < nums.length; i++) {
 9
-10        int currentMin = nums[0];
-11        int minSum = nums[0];
-12
-13        totalSum = nums[0];
+10            int current = nums[i];
+11
+12            int tempMax = Math.max(current,
+13                    Math.max(maxProduct * current, minProduct * current));
 14
-15        for (int i = 1; i < nums.length; i++) {
-16
-17            totalSum += nums[i];
-18
-19            currentMax = Math.max(nums[i], currentMax + nums[i]);
-20            maxSum = Math.max(maxSum, currentMax);
-21
-22            currentMin = Math.min(nums[i], currentMin + nums[i]);
-23            minSum = Math.min(minSum, currentMin);
-24        }
-25
-26        
-27        if (maxSum < 0)
-28            return maxSum;
-29
-30        return Math.max(maxSum, totalSum - minSum);
-31    }
-32}
+15            int tempMin = Math.min(current,
+16                    Math.min(maxProduct * current, minProduct * current));
+17
+18            maxProduct = tempMax;
+19            minProduct = tempMin;
+20
+21            ans = Math.max(ans, maxProduct);
+22        }
+23
+24        return ans;
+25    }
+26}
